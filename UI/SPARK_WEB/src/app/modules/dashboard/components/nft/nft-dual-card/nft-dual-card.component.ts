ea9cb332dadd34417a659803e9165a151ec6d7f4
abdullah <abdullah@mislbd.com>
2025-06-01 17:44:18 +0600

    UI template added

---
+++ UI/SPARK_WEB/src/app/modules/dashboard/components/nft/nft-dual-card/nft-dual-card.component.ts
@@ -0,0 +1,17 @@
+import { CurrencyPipe, NgStyle } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { Nft } from '../../../models/nft';
+
+@Component({
+  standalone: true,
+  selector: '[nft-dual-card]',
+  templateUrl: './nft-dual-card.component.html',
+  imports: [NgStyle, CurrencyPipe],
+})
+export class NftDualCardComponent implements OnInit {
+  @Input() nft: Nft = <Nft>{};
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ UI/SPARK_WEB/src/app/modules/dashboard/components/nft/nft-header/nft-header.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nft-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './nft-header.component.html',
+  styleUrls: ['./nft-header.component.css']
+})
+export class NftHeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
